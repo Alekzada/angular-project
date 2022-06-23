f49c9b581c9cde39390abf05bc94ebd911be81f1
alekzada <alexismolica.6a@hotmail.com>
2022-06-22 21:52:59 -0300

    site

--- angular/blogTheWorldGames/src/app/providers/blog.service.ts
+++ angular/blogTheWorldGames/src/app/providers/blog.service.ts
@@ -1,9 +1,31 @@
+// Usado para decorar os serviços Angular
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+// Importa Lodash como _
+import * as _ from 'lodash';
+
+// Usado para navegar entre rotas
+import { Router } from '@angular/router';
+
+// Usado para requisições GET, POST, ETC
+import { HttpClientModule } from '@angular/common/http';
+
+// Declara uma constante base URL
+const baseURL = window.location.href;
+
+// Decora a classe Blog Service para ser "injetável" dentro do Angular.
+@Injectable()
+
+// Exporta nossa classe para ser entendida por outros arquivos.
 export class BlogService {
 
-  constructor() { }
+// Declara uma variável de escopo global na classe, com a URL do serviço externo que iremos consumir. Por exemplo: podemos copiar essa URL do nosso navegador
+  getURL: string = 'https://jsonplaceholder.typicode.com/posts'
+
+  constructor(
+    public http: HttpClientModule, // Injeta o serviço HTTP do Angular
+    public router: Router, // Injeta o serviço do Router do Angular.
+  ) { }
 }
+
+// Pegar a lista de posts
